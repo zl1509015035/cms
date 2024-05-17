4cc8e0f4dc43f4bd377fca74201748ae851f3da2
ZhuLei <lei.zhu@reach-health.com.cn>
2024-05-17 15:10:01 +0800

    new file:   .env 	modified:   index.html 	new file:   jsconfig.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	deleted:    src/assets/vue.svg 	deleted:    src/components/HelloWorld.vue 	modified:   src/main.js 	new file:   src/router/index.js 	new file:   src/stores/admin/admin.js 	deleted:    src/style.css 	new file:   src/views/admin/login.vue 	modified:   vite.config.js

---
+++ src/views/admin/login.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import {useAdminStore} from '@/stores/admin/admin.js'
+
+const adminStore = useAdminStore()
+
+adminStore.save("juanwang","juanwang.com","2024-05-17")
+
+console.log(adminStore.data);
+</script>
+
+<template>
+    <h3>登录页</h3>
+
+    <el-icon><User/></el-icon>
+
+    <hr>
+
+    <el-button type="primary">登录</el-button>
+</template>
+
+<style scoped>
+
+</style>
