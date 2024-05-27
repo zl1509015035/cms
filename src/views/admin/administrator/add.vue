3d1bca67f64adbcb9a2058a31b7291e764f6db50
ZhuLei <lei.zhu@reach-health.com.cn>
2024-05-27 16:52:56 +0800

    添加管理员页面

--- src/views/admin/administrator/add.vue
+++ src/views/admin/administrator/add.vue
@@ -1,9 +1,64 @@
 <script setup>
 
+import {reactive} from "vue";
+
+const data = reactive({
+  name:'',
+  password:'',
+  email:'',
+  gender:'0',
+  remark:'juanwangcode.com'
+})
+
+const add = () =>{
+  console.log(data)
+}
+
+const reset = () =>{
+  data.name = ''
+  data.password = ''
+  data.email = ''
+  data.gender = ''
+  data.remark = ''
+}
+
+
+
 </script>
 
 <template>
-    <h3>add</h3>
+  <el-form label-width="80" style="width:400px">
+    <el-form-item label="名称">
+      <el-input v-model="data.name" placeholder="请填写名称"/>
+    </el-form-item>
+
+    <el-form-item label="密码">
+      <el-input v-model="data.password" show-password placeholder="请填写密码"/>
+    </el-form-item>
+
+    <el-form-item label="邮箱">
+      <el-input v-model="data.email"/>
+    </el-form-item>
+
+    <el-form-item label="备注">
+      <el-input type="textarea" v-model="data.remark" rows="4"/>
+    </el-form-item>
+
+    <el-form-item label="性别">
+      <el-radio-group v-model="data.gender">
+        <el-radio value="0">未知</el-radio>
+        <el-radio value="1">男</el-radio>
+        <el-radio value="2">女</el-radio>
+      </el-radio-group>
+    </el-form-item>
+
+
+    <el-form-item >
+      <el-button type="primary" @click="add">添加</el-button>
+      <el-button @click="reset">重置</el-button>
+    </el-form-item>
+  </el-form>
+
 </template>
 
 <style scoped></style>
