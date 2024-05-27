270cfea7862d631b0a0cee6b9ed87f79be2a3b32
zl1509015035 <1509015035@qq.com>
2024-05-27 22:31:03 +0800

    删除管理员及onMounted生命周期

--- src/views/admin/administrator/list.vue
+++ src/views/admin/administrator/list.vue
@@ -1,40 +1,84 @@
 <script setup>
-import {reactive} from "vue";
+import {reactive, onMounted} from "vue";
 import AxiosUtil from "@/Utils/AxiosUtil.js";
-import {ElMessage} from "element-plus";
+import {ElMessage, ElMessageBox} from "element-plus";
 
 
 const data = reactive({
-  list:[]
+  list: []
+})
+
+onMounted(() => {
+  AxiosUtil.get('/api/adm/list').then(result => {
+
+    console.log(result)
+
+    if (!result.status) {
+      ElMessage.error(result.msg)
+      return
+    }
+
+    data.list = result.data.list
+  }).catch(err => {
+    console.log("err:", err)
+  })
+
 })
 
 
-AxiosUtil.get('/api/adm/list').then(result =>{
+//删除
+/*const del = (row) => {
+    ElMessageBox.confirm('确认删除?', '标题', {
+        type: 'warning', // success | warning | info | error
+        confirmButtonText: '确认',
+        cancelButtonText: '取消'
+    }).then(() => {
+        console.log('确认删除 id:',row.id)
+    }).catch(() => {
+        console.log('取消删除')
+    })
+}*/
+const del = async (row) => {
+  try {
+    await ElMessageBox.confirm('确认删除?', '标题', {
+      type: 'warning',
+      confirmButtonText: '确认',
+      cancelButtonText: '取消'
+    })
 
-  console.log(result)
+    //删除
+    //let delResult = await AxiosDR.post('/api/adm/del',{id: row.id})
+    let delResult = await AxiosUtil.post('/api/adm/del', {id: String(row.id)})
+    if (!delResult.status) {
+      ElMessage.error(delResult.msg)
+      return
+    }
 
-  if(!result.status){
-    ElMessage.error(result.msg)
-    return
+    //重新获取列表
+    let getListResult = await AxiosUtil.get('/api/adm/list')
+    if (!getListResult.status) {
+      ElMessage.error(getListResult.msg)
+      return
+    }
+
+    data.list = getListResult.data.list //重置
+  } catch (err) {
+    console.log("err:", err)
   }
-
-  data.list = result.data.list
-}).catch(err =>{
-  console.log("err:",err)
-})
+}
 </script>
 
 <template>
   <el-table :data="data.list" border>
-    <el-table-column prop="id" label="ID" width="60" />
-    <el-table-column prop="name" label="名称" />
-    <el-table-column prop="email" label="邮箱" />
+    <el-table-column prop="id" label="ID" width="60"/>
+    <el-table-column prop="name" label="名称"/>
+    <el-table-column prop="email" label="邮箱"/>
     <el-table-column prop="remark" label="备注"/>
 
     <el-table-column label="操作" width="150">
       <template #default="scope">
         <el-button size="small" type="primary">编辑</el-button>
-        <el-button size="small">删除</el-button>
+        <el-button size="small" @click="del(scope.row)">删除</el-button>
       </template>
     </el-table-column>
   </el-table>
