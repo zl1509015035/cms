cfd6b4b315cf866bf6c79ace86f6a20d80c747ce
ZhuLei <lei.zhu@reach-health.com.cn>
2024-05-30 17:51:30 +0800

    类别页面布局

--- src/views/admin/category/list.vue
+++ src/views/admin/category/list.vue
@@ -1,9 +1,67 @@
 <script setup>
+import {reactive} from 'vue'
+
+//数据
+const data = reactive({
+  path: [
+    {id: '1', name: '类别', parent_id: '0', create_time: '2024-03-22'},
+    {id: '2', name: '前端', parent_id: '1', create_time: '2024-03-23'}
+  ],
+  list: [
+    {id: '3', name: 'Vue', parent_id: '2', status: '1', create_time: '2024-03-23'},
+    {id: '4', name: 'ES', parent_id: '2', status: '1', create_time: '2024-03-24'},
+    {id: '5', name: 'JS', parent_id: '2', status: '0', create_time: '2024-03-25'}
+  ]
+})
 
 </script>
 
 <template>
-    <h3>类别类目管理</h3>
+  <!-- 面包屑
+  <el-breadcrumb separator="/">
+      <el-breadcrumb-item><a href="#"><el-icon><House /></el-icon></a></el-breadcrumb-item>
+      <el-breadcrumb-item><a href="#">类别</a></el-breadcrumb-item>
+      <el-breadcrumb-item><a href="#">前端</a></el-breadcrumb-item>
+  </el-breadcrumb>
+  -->
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item>
+      <a href="/admin/category/list?parent_id=0"><el-icon><House /></el-icon></a>
+    </el-breadcrumb-item>
+
+    <el-breadcrumb-item v-for="value in data.path" :key="value.id">
+      <a :href="`/admin/category/list?parent_id=${value.id}`">{{ value.name }}</a>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+
+  <!--  按钮-->
+  <el-button type="primary" size="small">添加类别</el-button>
+
+  <!--  表格-->
+  <el-table :data="data.list" border>
+    <el-table-column prop="id" label="ID" width="80"/>
+<!--    <el-table-column prop="name" label="名称"/>-->
+    <el-table-column prop="name" label="名称">
+      <template #default="scope">
+        <a :href="`/admin/category/list?parent_id=${scope.row.id}`" class="blue">{{ scope.row.name }}</a>
+      </template>
+    </el-table-column>
+<!--    <el-table-column prop="status" label="状态" width="80"/>-->
+    <el-table-column prop="status" label="状态" width="80">
+      <template #default="scope">
+        <span v-if="scope.row.status === '1'" class="green">显示</span>
+        <span v-else class="orange">隐藏</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="create_time" label="日期" width="200"/>
+
+    <el-table-column label="操作" width="150">
+      <template #default="scope">
+        <el-button size="small" type="primary">编辑</el-button>
+        <el-button size="small">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <style scoped></style>
